c22c8e1da00bec368cf5d1992fb1e6445219b37e
MiguelCo01 <miguelmelocosta@gmail.com>
2024-08-28 20:51:44 +0100

    C02 ex06

--- C02/ex06/test.c
+++ C02/ex06/test.c
@@ -6,7 +6,7 @@
 /*   By: mimelo-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 22:58:34 by mimelo-d          #+#    #+#             */
-/*   Updated: 2024/08/25 20:05:48 by mimelo-d         ###   ########.fr       */
+/*   Updated: 2024/08/28 20:45:56 by mimelo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ int	ft_str_is_printable(char *str);
 int	main(TEST_ARGS)
 {
 	BEGIN_TEST;
-	TEST(assert_int(ft_str_is_printable(str), 
+	TEST(assert_int(ft_str_is_printable("abc"), 1, "Checking 'abc'"));	
+	TEST(assert_int(ft_str_is_printable(""), 1, "Checking ''"));	
+	TEST(assert_int(ft_str_is_printable("\n"), 0, "Checking '\\n'"));	
+	TEST(assert_int(ft_str_is_printable("\nab"), 0, "Checking '\\nab'"));	
 	END_TEST;
 }
